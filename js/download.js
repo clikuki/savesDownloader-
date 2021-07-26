fa02fe80345e1ad0098e0d8d05de9e98b4fb8ccd
Clikuki <73450123+clikuki@users.noreply.github.com>
2021-07-26 21:26:34 +0800

    now downloads urls

--- js/download.js
+++ js/download.js
@@ -3,16 +3,12 @@ const path = require('path');
 const fs = require('fs');
 
 const download = async (url, dest, autoFileName = true) => {
+	if(autoFileName) dest += path.basename(url);
+
 	const res = await fetch(url);
-
-	if(autoFileName)
-	{
-		dest += path.basename(url);
-	}
-
 	const fileStream = fs.createWriteStream(dest);
 
-	await new Promise((resolve, reject) => {
+	return new Promise((resolve, reject) => {
 		res.body.pipe(fileStream);
 		res.body.on("error", reject);
 		fileStream.on("finish", resolve);
--- js/index.js
+++ js/index.js
@@ -56,7 +56,9 @@ function getUrls(limit)
 					break;
 
 				case 'video':
-					data.vidUrl = thing.secure_media.reddit_video.fallback_url;
+					data.vidUrl = thing.secure_media.reddit_video.fallback_url
+									.replace('?source=fallback', '');
+
 					data.audioUrl = data.vidUrl.replace(/(?<=DASH_)[0-9]*/, 'audio');
 					break;
 
@@ -81,15 +83,77 @@ prompt([
 ], ({ fetchLimit }) =>
 {
 	getUrls(+fetchLimit || 10)
-		.then(listing =>
-		{// listing contains some properties that could get iterated, so you need to deep copy the listing
-			for(const [index, thing] of Object.entries([...listing]))
+		// .then(listing => chunkify([...listing], 10))
+		.then(async (listing) =>
+		{	
+			const uniqueStr = () =>
 			{
-				console.log(`${index}:`, thing);
+				const getRandomInt = (min, max) =>
+				{
+					min = Math.ceil(min);
+					max = Math.floor(max);
+				
+					const randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+				
+					return randomInt;
+				}
+
+				const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+				const charactersLength = characters.length;
+			
+				let result = '';
+				for (let i = 0; i < getRandomInt(10, 20); i++)
+				{
+					result += characters.charAt(Math.floor(Math.random() * charactersLength));
+				}
+			
+				return result;
+			};
+
+			const dest = './downloads/'
+			for(const data of listing)
+			{
+				switch(data.type)
+				{
+					case 'comment':
+					case 'text':
+						// Skip over them because they don't have files that can be downloaded,
+						// and regex-ing over them to get image/video urls is difficult
+						break;
+
+					case 'image':
+						await download(data.url, dest);
+						break;
+
+					case 'gallery':
+						{
+							for(const url of data.urlArray)
+							{
+								await download(url, dest);
+							}
+						}
+						break;
+
+					case 'video':
+						{
+							const dest = './downloads/';
+							const newDest = `${dest}${uniqueStr()}/`;
+						
+							await fs.promises.mkdir(newDest)
+							await download(data.vidUrl, newDest);
+							await download(data.audioUrl, newDest);
+						}
+						break;
+				}
 			}
 		})
 })
 
+// listing contains some properties that could get iterated, so you need to deep copy the listing
+// for(const [index, thing] of Object.entries([...listing]))
+// {
+// 	console.log(`${index}:`, thing);
+// }
 // console.log(listing.length);
 // fs.writeFileSync('./downloads/image.json', JSON.stringify(listing));
 // download('https://jsonplaceholder.typicode.com/users/', './downloads/test.json');
