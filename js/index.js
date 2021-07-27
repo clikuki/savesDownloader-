711e63354fe2a14c8550b75fbcb7e831b3880ace
Clikuki <73450123+clikuki@users.noreply.github.com>
2021-07-27 10:53:29 +0800

    chunky downloads!
    Downloads in chunks(currently set to 2) instead of doing it one by one
    added some comments
    moved starter callback into its function

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const getThingType = require('./getThingType');
 const prompt = require('./prompt');
 const download = require('./download');
+const { resolve } = require('path');
 
 const userInfo = (() =>
 { // load dev or user credentials
@@ -22,6 +23,7 @@ const r = new Snoowrap({
 	password: userInfo.PASSWORD,
 })
 
+// fetch saves
 function fetchSaves(limit)
 {
 	if(limit === 'all')
@@ -32,6 +34,7 @@ function fetchSaves(limit)
 	return r.getMe().getSavedContent({ limit });
 }
 
+// takes the needed data from the listing and return it
 function getUrls(limit)
 {
 	return fetchSaves(limit).then(listing =>
@@ -75,85 +78,101 @@ function getUrls(limit)
 	})
 }
 
+// returns a diffrent promise for each type of save
+function downloadSave(data)
+{
+	const uniqueStr = () =>
+	{
+		const getRandomInt = (min, max) =>
+		{
+			min = Math.ceil(min);
+			max = Math.floor(max);
+		
+			const randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+		
+			return randomInt;
+		}
+
+		const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		const charactersLength = characters.length;
+	
+		let result = '';
+		for (let i = 0; i < getRandomInt(10, 20); i++)
+		{
+			result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		}
+	
+		return result;
+	};
+
+	const dest = './downloads/'
+	switch(data.type)
+	{
+		case 'comment':
+		case 'text':
+			// Skip over them because they don't have files that can be downloaded,
+			// and regex-ing over them to get image/video urls is difficult
+			break;
+
+		case 'image':
+			return download(data.url, dest);
+
+		case 'gallery':
+			{ // not finalized, kinda just put this part together
+				return new Promise(async () =>
+				{
+					for(const url of data.urlArray)
+					{
+						await download(url, dest);
+					}
+				})
+			}
+			break;
+
+		case 'video':
+			{
+				const newDest = `${dest}${uniqueStr()}/`;
+			
+				return async () =>
+				{
+					await fs.promises.mkdir(newDest)
+					await download(data.vidUrl, newDest);
+					await download(data.audioUrl, newDest);
+				}
+			}
+	}
+}
+
+// Split array into chunks/arrays within an array
+function chunkify(array, chunkLength)
+{
+	const newArr = []
+	for (let i = 0; i < array.length; i += chunkLength) {
+		newArr.push(array.slice(i, i + chunkLength));
+	}
+
+	return newArr;
+}
+
+// callback that is called when fetch limit prompt has been answered
+function startCallback({ fetchLimit })
+{
+	getUrls(+fetchLimit || 10)
+		.then(listing => chunkify([...listing], 2))
+		.then(async (chunkyListing) =>
+		{	
+			for(const chunk of chunkyListing)
+			{
+				const promiseArray = chunk.map(data => downloadSave(data));
+				await Promise.all(promiseArray);
+			}
+		})
+}
+
+// start program with fetch limit prompt
 prompt([
 	{
 		question: 'How many saves do you want to fetch?',
 		key: 'fetchLimit', 
 	}
-], ({ fetchLimit }) =>
-{
-	getUrls(+fetchLimit || 10)
-		// .then(listing => chunkify([...listing], 10))
-		.then(async (listing) =>
-		{	
-			const uniqueStr = () =>
-			{
-				const getRandomInt = (min, max) =>
-				{
-					min = Math.ceil(min);
-					max = Math.floor(max);
-				
-					const randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
-				
-					return randomInt;
-				}
-
-				const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-				const charactersLength = characters.length;
-			
-				let result = '';
-				for (let i = 0; i < getRandomInt(10, 20); i++)
-				{
-					result += characters.charAt(Math.floor(Math.random() * charactersLength));
-				}
-			
-				return result;
-			};
-
-			const dest = './downloads/'
-			for(const data of listing)
-			{
-				switch(data.type)
-				{
-					case 'comment':
-					case 'text':
-						// Skip over them because they don't have files that can be downloaded,
-						// and regex-ing over them to get image/video urls is difficult
-						break;
-
-					case 'image':
-						await download(data.url, dest);
-						break;
-
-					case 'gallery':
-						{
-							for(const url of data.urlArray)
-							{
-								await download(url, dest);
-							}
-						}
-						break;
-
-					case 'video':
-						{
-							const dest = './downloads/';
-							const newDest = `${dest}${uniqueStr()}/`;
-						
-							await fs.promises.mkdir(newDest)
-							await download(data.vidUrl, newDest);
-							await download(data.audioUrl, newDest);
-						}
-						break;
-				}
-			}
-		})
-})
-
-// listing contains some properties that could get iterated, so you need to deep copy the listing
-// for(const [index, thing] of Object.entries([...listing]))
-// {
-// 	console.log(`${index}:`, thing);
-// }
-// console.log(listing.length);
-// fs.writeFileSync('./downloads/image.json', JSON.stringify(listing));
-// download('https://jsonplaceholder.typicode.com/users/', './downloads/test.json');
+], startCallback)
